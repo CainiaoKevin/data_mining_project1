5352f9d3d41c3db5cbe357b9a008082383614ed2
Kefeng Shi <kefeng.shi@climate.com>
2019-03-13 16:34:02 -0500

    read data

--- Santander_customer_transaction_prediction.R
+++ Santander_customer_transaction_prediction.R
@@ -1,3 +1,4 @@
 
-print('hello world')
-
+library(dplyr)
+train_data <- read.csv('/Users/kefeng.shi/Downloads/santander-customer-transaction-prediction/train.csv')
+test_data <- read.csv('/Users/kefeng.shi/Downloads/santander-customer-transaction-prediction/test.csv')
